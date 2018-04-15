bfa738f6d5e3efa341ddacc7b1f8e5ca0a6bad4b
Karol <naerriel@gmail.com>
2018-04-15 10:40:58 +0200

    Add Transaction and Searching components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,83 @@
 import React, { Component } from 'react';
 import './App.css';
 
+class Transaction extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      hash: props.hash
+    };
+  }
+  unmount = () => {
+    this.props.unmount();
+  }
+  render(){
+    return (
+      <div className="transaction">
+        <p>My hash is:{this.state.hash}</p>
+        <button onClick={this.unmount}>Go back</button>
+      </div>
+    );
+  }
+}
+
+class Searching extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      hashValue: '0'
+    }
+  }
+  clickSearch = () => {
+    this.props.handleSearch(this.state.hashValue);
+  }
+  handleTextAreaChange = (e) => {
+    this.setState({hashValue: e.target.value});
+  }
+  render() {
+    return (
+      <div className="searchBox">
+        <p>Search for transaction:</p>
+        <textarea onChange={this.handleTextAreaChange} placeholder="Transaction hash..."></textarea>
+        <button className="searchButton" onClick={this.clickSearch}>Search</button>
+      </div>
+    );
+  }
+}
+
 class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      renderSearch: true,
+      transactionHash: "0"
+    };
+  }
+  handleSearch = (transactionHash) => {
+    console.log(transactionHash);
+    this.setState({
+      renderSearch: false,
+      transactionHash: transactionHash
+    });
+  }
+  unmountTransaction = () => {
+    this.setState({
+      renderSearch: true,
+    });
+  }
   render() {
     return (
       <div className="App">
-        Hello
+        {this.state.renderSearch ?
+          <Searching
+            handleSearch={this.handleSearch}
+          />
+        :
+          <Transaction
+            hash={this.state.transactionHash}
+            unmount={this.unmountTransaction}
+          />
+        }
       </div>
     );
   }
