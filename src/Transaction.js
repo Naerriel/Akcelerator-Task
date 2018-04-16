439b040d94b9a8ea9c80c7c7fbe75268c550e254
Karol <naerriel@gmail.com>
2018-04-16 21:11:15 +0200

    Code review

--- src/Transaction.js
+++ src/Transaction.js
@@ -7,8 +7,8 @@ const satoshiToBTC = (satoshiNum) => {
   return BTCNum + " BTC";
 }
 
-const formatDate = (date) => {
-  return date.replace("T", " ").slice(0, -5);
+const formatDate = (iso8601_date) => {
+  return iso8601_date.replace("T", " ").slice(0, -5);
 }
 
 class DataTable extends Component {
@@ -20,7 +20,7 @@ class DataTable extends Component {
 
   render(){
     let rows = [];
-    for(let rowNum = 0; rowNum < this.state.table.length; rowNum++){
+    for(let rowNum in this.state.table){
       const description = this.state.table[rowNum][0];
       const value = this.state.table[rowNum][1];
       rows.push(<dl><dd>{description}</dd><dt>{value}</dt></dl>);
@@ -32,63 +32,73 @@ class DataTable extends Component {
 }
 
 class Transaction extends Component {
+  totalInput = 0;
+
   constructor(props){
     super(props);
 
-    let inputs = [];
-    let outputs = [];
-    let totalInput = 0;
+    const inputs = this._init_inputs(props.json.inputs);
+    const outputs = this._init_outputs(props.json.outputs);;
     const feesRate =
       satoshiToBTC(props.json.fees * kilo / props.json.size) + "/KB";
 
-    if(props.json.inputs[0].hasOwnProperty("addresses")){
-      for(let inNr in props.json.inputs){
-        const address = props.json.inputs[inNr].addresses[0];
-        const value = props.json.inputs[inNr].output_value;
-
-        inputs.push(<li><span className="left">{address}</span>
-          <span className="right">{satoshiToBTC(value)}</span></li>);
-        totalInput += value;
-      }
-    }
-    else{
-      inputs.push(<li><span className="left">No inputs (new coins generated)</span></li>);
-    }
-
-    for(let outNr in props.json.outputs){
-      let address;
-      if(props.json.outputs[outNr].addresses){ // Adress can be null
-        address = props.json.outputs[outNr].addresses[0];
-      }
-      else{
-        address = "Unparsed address";
-      }
-      const value = satoshiToBTC(props.json.outputs[outNr].value);
-
-      outputs.push(<li><span className="left">{address}</span>
-        <span className="right">{value}</span></li>);
-    }
-
-    const firstTable = [
-      ["Total value", satoshiToBTC(props.json.total)],
+    const mainInfoTable = [
+      ["Total value:", satoshiToBTC(props.json.total)],
       ["Confirmations:", props.json.confirmations],
       ["Block:", props.json.block_height],
       ["Received:", formatDate(props.json.received)]];
-    const secondTable = [
-      ["Total inputs:", satoshiToBTC(totalInput)],
+    const technicalInfoTable = [
+      ["Total inputs:", satoshiToBTC(this.totalInput)],
       ["Size:", props.json.size + " (Bytes)"],
       ["Fees:", satoshiToBTC(props.json.fees)],
       ["Fees rate:", feesRate]];
 
     this.state = {
       hash: props.json.hash,
-      firstTable: firstTable,
-      secondTable: secondTable,
+      mainInfoTable: mainInfoTable,
+      technicalInfoTable: technicalInfoTable,
       inputs: inputs,
       outputs: outputs,
-      totalInput: satoshiToBTC(totalInput),
+      totalInput: satoshiToBTC(this.totalInput),
       totalOutput: satoshiToBTC(props.json.total)
     };
+  }
+
+  _init_inputs = (inputs) => {
+    const parsedInputs = [];
+    if(inputs[0].hasOwnProperty("addresses")){
+      for(let inNr in inputs){
+        const address = inputs[inNr].addresses[0];
+        const value = inputs[inNr].output_value;
+
+        parsedInputs.push(<li><span className="left">{address}</span>
+          <span className="right">{satoshiToBTC(value)}</span></li>);
+        this.totalInput += value;
+      }
+    }
+    else{
+      parsedInputs.push(<li>
+        <span className="left">No inputs (new coins generated)</span></li>);
+    }
+    return parsedInputs;
+  }
+
+  _init_outputs = (outputs) => {
+    const parsedOutputs = [];
+    for(let outNr in outputs){
+      let address;
+      if(outputs[outNr].addresses){ // Adress can be null
+        address = outputs[outNr].addresses[0];
+      }
+      else{
+        address = "Unparsed address";
+      }
+      const value = satoshiToBTC(outputs[outNr].value);
+
+      parsedOutputs.push(<li><span className="left">{address}</span>
+        <span className="right">{value}</span></li>);
+    }
+    return parsedOutputs;
   }
 
   unmount = () => {
@@ -103,8 +113,8 @@ class Transaction extends Component {
           <span className="transaction-hash">{this.state.hash}</span>
         </div>
         <div className="details">
-          <DataTable table={this.state.firstTable}/>
-          <DataTable table={this.state.secondTable}/>
+          <DataTable table={this.state.mainInfoTable}/>
+          <DataTable table={this.state.technicalInfoTable}/>
         </div>
         <div className="addresses">
           <div className="addresses-header">
